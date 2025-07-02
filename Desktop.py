ae6cfc1ceb1e161964c965dfec2824412aaf5e6e
Bhumi goyal <130773299+bhumi223344@users.noreply.github.com>
2025-07-02 17:12:42 +0530

    Add files via upload

---
+++ Desktop.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:**
+# 
+# - List popular languages for Data Science
+# - List commonly used libraries for Data Science
+# - Create tables using Markdown
+
+# Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R
+# 3. SQL
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. Numpy
+# 2. Pandas
+# 3. MatPlotLib
+# 
+# 
+
+# | Data Science Tools |
+# |---------------------|
+# | Jupyter Notebook    |
+# | RStudio             |
+# | Apache Zeppelin     |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[3]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[5]:
+
+
+# This will convert 200 minutes to hours by dividing by 60
+M = 200
+H = M / 60
+print("200 minutes is equal to", H, "hours")
+
+
+# ## Author
+# 
+# Bhumi Goyal
+
+# In[ ]:
+
+
+
+
